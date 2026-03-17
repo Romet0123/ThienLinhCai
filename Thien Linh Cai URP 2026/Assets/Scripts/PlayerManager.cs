ae74b77cce9b244750154ddb40112fad8db646e2
Sieunhanom15 <87540541+Romet0123@users.noreply.github.com>
2026-03-18 01:11:40 +0700

    Doing Suicide Ghost

--- Thien Linh Cai URP 2026/Assets/Scripts/PlayerManager.cs
+++ Thien Linh Cai URP 2026/Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:575d4b69ccd549f233c42d8b0fd9750662b4a5789b8ff9dea550aa9f0c8c32c6
-size 141486
+oid sha256:1739122c7dcf9d335f5b9c69590668d96f1784499a789a80ca4d4dcc2c51ca4a
+size 145352
